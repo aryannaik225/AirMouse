90a84d93f4b8e5b1488ac8e13c939076f6222ecc
aryannaik225 <aryann2203@gmail.com>
2025-05-16 11:50:00 +0530

    created tailwind config file

---
+++ airmouse/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}", // adjust this based on your structure
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
